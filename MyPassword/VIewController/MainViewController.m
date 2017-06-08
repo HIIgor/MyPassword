f9549267fe07eb4b847d370376816034b254a185
JvanChow <zoujianfeng@renrenche.com>
2017-06-08 15:03:23 +0800

    MOD：主界面右上角增加添加按钮

--- MyPassword/VIewController/MainViewController.m
+++ MyPassword/VIewController/MainViewController.m
@@ -8,6 +8,8 @@
 
 #import "MainViewController.h"
 #import "UIBarButtonItem+Extension.h"
+#import "UIView+position.h"
+#import <Masonry/Masonry.h>
 
 @interface MainViewController ()
 
@@ -57,11 +59,15 @@
 #pragma mark - Private
 
 - (void)leftAction {
-
+    NSLog(@"leftAction");
 }
 
 - (void)rightAction {
+    NSLog(@"rightAction");
+}
 
+- (void)addAction {
+    NSLog(@"addAction");
 }
 
 #pragma mark - Public
@@ -78,14 +84,19 @@
 
 - (UIBarButtonItem *)rightBarButtonItem {
     if (!_rightBarButtonItem) {
-//        UIButton *rightButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
-//        rightButton.titleLabel.font = [UIFont systemFontOfSize:17];
-//        [rightButton setTitle:@"" forState:UIControlStateNormal];
-//        [rightButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//        [rightButton addTarget:self action:@selector(clearAction:) forControlEvents:UIControlEventTouchUpInside];
-//
-        //        _rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightButton];
         _rightBarButtonItem = [UIBarButtonItem rightBarButtonItemWithText:@"导出" target:self action:@selector(rightAction)];
+        _rightBarButtonItem.customView.frameWidth = 130;
+
+        UIButton *addButton = [[UIButton alloc] initWithFrame:CGRectMake(86, 0, 44, 44)];
+        addButton.titleLabel.font = [UIFont systemFontOfSize:17];
+        [addButton setTitle:@"添加" forState:UIControlStateNormal];
+        [addButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [addButton addTarget:self action:@selector(addAction) forControlEvents:UIControlEventTouchUpInside];
+        [addButton setTitleEdgeInsets:UIEdgeInsetsMake(-1, 0, 0, 0)];
+
+        CGSize size = [@"添加" sizeWithAttributes:@{NSFontAttributeName : [UIFont systemFontOfSize:17],}];
+        addButton.frameWidth = size.width + 6;
+        [_rightBarButtonItem.customView addSubview:addButton];
     }
 
     return _rightBarButtonItem;
