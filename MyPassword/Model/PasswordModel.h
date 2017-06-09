e56d2879f43ce203c2847e5bc0e992272071a18a
JvanChow <zoujianfeng@renrenche.com>
2017-06-09 14:48:32 +0800

    MOD：create model file. remove useless file.

---
+++ MyPassword/Model/PasswordModel.h
@@ -0,0 +1,19 @@
+//
+//  PasswordModel.h
+//  MyPassword
+//
+//  Created by JvanChow on 08/06/2017.
+//  Copyright © 2017 JvanChow. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+#import <UIKit/UIKit.h>
+
+@interface PasswordModel : RLMObject
+
+@property NSInteger number;
+@property NSString *title;
+@property NSString *password;
+@property NSString *detailDescription;
+
+@end
---
+++ MyPassword/Model/PasswordModel.m
@@ -0,0 +1,17 @@
+//
+//  PasswordModel.m
+//  MyPassword
+//
+//  Created by JvanChow on 08/06/2017.
+//  Copyright © 2017 JvanChow. All rights reserved.
+//
+
+#import "PasswordModel.h"
+
+@implementation PasswordModel
+
++ (NSString *)primaryKey {
+    return @"number";
+}
+
+@end
